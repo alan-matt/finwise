95d35ff2ae0c09c0551e6e812cac0a56b21411da
paulwalker721 <183711928+paulwalker721@users.noreply.github.com>
2025-05-23 04:08:24 -0700

    Add GoalsModel class with JSON serialization and update dependencies

---
+++ lib/models/goals_model.dart
@@ -0,0 +1,73 @@
+class GoalsModel {
+  String? status;
+  String? message;
+  List<Data>? data;
+
+  GoalsModel({this.status, this.message, this.data});
+
+  GoalsModel.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    message = json['message'];
+    if (json['data'] != null) {
+      data = <Data>[];
+      json['data'].forEach((v) {
+        data!.add(new Data.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['status'] = this.status;
+    data['message'] = this.message;
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Data {
+  String? sId;
+  String? title;
+  String? month;
+  int? year;
+  int? amount;
+  String? createdAt;
+  String? updatedAt;
+  int? iV;
+
+  Data(
+      {this.sId,
+      this.title,
+      this.month,
+      this.year,
+      this.amount,
+      this.createdAt,
+      this.updatedAt,
+      this.iV});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    title = json['title'];
+    month = json['month'];
+    year = json['year'];
+    amount = json['amount'];
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
+    iV = json['__v'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['_id'] = this.sId;
+    data['title'] = this.title;
+    data['month'] = this.month;
+    data['year'] = this.year;
+    data['amount'] = this.amount;
+    data['createdAt'] = this.createdAt;
+    data['updatedAt'] = this.updatedAt;
+    data['__v'] = this.iV;
+    return data;
+  }
+}
